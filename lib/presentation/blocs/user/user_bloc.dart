492a45b7c7db21594fa1b6049528ee391d2fdb1d
Lucas Farias <lucasfarias.ln@gmail.com>
2026-01-13 22:27:15 -0300

    feat: refactor user management components and add error/skeleton states

--- lib/presentation/blocs/user/user_bloc.dart
+++ lib/presentation/blocs/user/user_bloc.dart
@@ -12,47 +12,34 @@ class UserBloc extends Bloc<UserEvent, UserState> {
   final UserService _userService = UserServiceRemote();
 
   UserBloc() : super(UserInitial()) {
-    print('UserBloc criado!');
     on<GetUserRequested>(_onGetUserRequested);
-    print('Handler registrado para GetUserRequested');
   }
 
   Future<void> _onGetUserRequested(
     GetUserRequested event,
     Emitter<UserState> emit,
   ) async {
-    print('_onGetUserRequested CHAMADO!');
     emit(UserLoading());
 
     try {
       final token = TokenStorage.getToken();
-      print('Token encontrado: ${token != null ? "SIM" : "NÃO"}');
 
       if (token == null) {
         emit(UserError('Token não encontrado. Faça login novamente.'));
         return;
       }
 
-      print('Fazendo requisição para /user...');
       final response = await _userService.getUserById();
-
-      print('Status Code: ${response.statusCode}');
-      print('Response Data: ${response.data}');
 
       if (response.statusCode == 200) {
         final user = User.fromJson(response.data);
-        print('User criado com sucesso: ${user.email}');
         emit(UserSuccess(user));
       } else {
-        print(
-            'Erro na resposta: ${response.statusCode} - ${response.statusMessage}');
         emit(UserError(
             'Erro ${response.statusCode}: ${response.statusMessage}'));
       }
     } catch (e) {
-      print('Exception capturada: $e');
       final errorMessage = ErrorHandler.getErrorMessage(e);
-      print('Mensagem de erro tratada: $errorMessage');
       emit(UserError(errorMessage));
     }
   }
