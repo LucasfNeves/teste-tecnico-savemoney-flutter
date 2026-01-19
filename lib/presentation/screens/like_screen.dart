b85a08d5b0bb62890c9b82b96478f9ce24f28d73
lucasfNeves <lucasfarias.ln@gmail.com>
2026-01-19 10:07:04 -0300

    feat: replace ElevatedButton with SecundaryButton and update PrimaryButton in LikeScreen

--- lib/presentation/screens/like_screen.dart
+++ lib/presentation/screens/like_screen.dart
@@ -1,9 +1,9 @@
 // lib/presentation/screens/like_screen.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:teste_create_flutter/core/theme/app_theme.dart';
 import 'package:teste_create_flutter/presentation/blocs/counter/counter_bloc.dart';
 import 'package:teste_create_flutter/shared/components/like_card.dart';
+import 'package:teste_create_flutter/shared/components/primary_button.dart';
 import 'package:teste_create_flutter/shared/components/secundary_button.dart';
 import 'package:teste_create_flutter/shared/layouts/main_layout.dart';
 
@@ -24,25 +24,17 @@ class LikeScreen extends StatelessWidget {
                   isFullScreen: true,
                 ),
                 const SizedBox(height: 12),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton.icon(
-                    onPressed: () {
-                      context.read<CounterBloc>().reset();
-                    },
-                    icon: const Icon(Icons.refresh),
-                    label: const Text('Resetar'),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: AppTheme.cardBorderColor,
-                      foregroundColor: AppTheme.textPrimary,
-                      padding: const EdgeInsets.symmetric(vertical: 16),
-                    ),
-                  ),
+                SecundaryButton(
+                  onPressed: () {
+                    context.read<CounterBloc>().reset();
+                  },
+                  icon: Icons.refresh,
+                  label: 'Resetar',
                 ),
                 const SizedBox(height: 12),
-                SecundaryButton(
+                PrimaryButton(
                     icon: Icons.home,
-                    label: 'Voltar para a Home',
+                    text: 'Voltar para a Home',
                     onPressed: () =>
                         Navigator.pushNamed(context, '/users-list')),
               ],
